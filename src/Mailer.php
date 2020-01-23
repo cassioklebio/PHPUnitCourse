4bd934172f3cac4e845625167353de1a5d893b63
cassio klebio <cassiotr2009@gmail.com>
2020-01-23 09:35:18 -0300

    Customise the creation of the mock object: the getMockBuilder method

--- src/Mailer.php
+++ src/Mailer.php
@@ -12,6 +12,12 @@
          */
         public function sendMessage($email, $message)
         {
+            if(empty($email))
+            {
+                throw new Exception;
+            }
+
+
             //Use mail() or PHPMailer for example
             sleep(3);
 
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -44,5 +44,26 @@ class UserTest extends TestCase
         $this->assertTrue($user->notify("Hello"));
     }
 
+    public function testCannotNotifyUserWithNoEmail()
+    {
+        $user = new User;
+
+//        $mock_mailer = $this->createMock(Mailer::class);
+        $mock_mailer = $this->getMockBuilder(Mailer::class)
+                            ->setMethods(null) //deprecated
+                            ->getMock();
+
+//        $mock_mailer->method('sendMessage')
+//                    ->will($this->throwException(new Exception));
+
+
+
+        $user->setMailer($mock_mailer);
+
+        $this->expectException(Exception::class);
+
+        $user->notify("Hello");
+    }
+
 
 }
